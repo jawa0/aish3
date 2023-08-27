178f34595540a3dc195a3ae15557569e2a83cb2b
Jaba Adams <jaba.adams@gmail.com>
2023-08-27 10:22:41 -0400

    Renamed test csv file

--- aish3.py
+++ aish3.py
@@ -82,7 +82,7 @@ def run(fullscreen, width, height):
     # gui.content().add_child(label)
     # gui.content().add_child(text)
 
-    filename = "./data/IB-hist-TQQQ-2022-02-08-5s.csv"
+    filename = "./data/test1.csv"
     # filename = "./data/IB-hist-TQQQ-2023-01-20-5s.csv"
     sticks = gui.create_control("CandlestickPlot", csv_filepath=filename, x=10, y=40, w=1380, h=600)
     gui.content().add_child(sticks)
