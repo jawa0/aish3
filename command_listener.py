b0558516bd5ee0f03a383b92ebbd6b3b86ee4230
Jaba Adams <jaba.adams@gmail.com>
2023-11-08 19:21:56 -0500

    FIx crash when voice in enabled, but no LLM is set up. It was caused by command-detection.

--- command_listener.py
+++ command_listener.py
@@ -1,3 +1,18 @@
+# Copyright 2023 Jabavu W. Adams
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 import logging
 import queue
 from session import ChatCompletionHandler, Session
@@ -7,6 +22,7 @@ from typing import Callable
 class VoiceCommandListener:
     def __init__(self, session: Session, on_command: Callable[[str], None]):
         self.session = session
+
         self.completion_handler = ChatCompletionHandler(start_handler=self._chat_completion_start,
                                                         chunk_handler=self._chat_completion_chunk,
                                                         done_handler=self._chat_completion_done)
