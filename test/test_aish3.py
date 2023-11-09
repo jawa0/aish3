de99d67fb1142fbfaa7b72c5ad580a1e5295d281
Jaba Adams <jaba.adams@gmail.com>
2023-11-08 20:59:23 -0500

    Fixed unit tests and some coordinate debugging changes

--- test/test_aish3.py
+++ test/test_aish3.py
@@ -1,11 +1,15 @@
-import sys
+from dotenv import load_dotenv
+load_dotenv()
+
 import os
+import sys
+import unittest
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-import unittest
-from text_edit_buffer import TextEditBuffer
 from gui import GUI, GUIContainer, GUIControl
 from session import Session
+from text_edit_buffer import TextEditBuffer
 
 
 class TestTextEditBuffer(unittest.TestCase):
@@ -193,14 +197,19 @@ class TestTextEditBuffer(unittest.TestCase):
         
 
     def test_parented_control_coordinates(self):
+        # print(f'test_parented_control_coordinates')
         s = Session()
         g = GUI(renderer=None, font_descriptor=None, client_session=s)
 
+        # print('create GUIContainer c0')
         c0 = GUIContainer(gui=g)
+        # print('add c0 to g.content()')
         g.content().add_child(c0)
         c0.set_position(5, 7)
 
+        # print('create GUIControl c')
         c = GUIControl(gui=g)
+        # print('add c to c0')
         c0.add_child(c)
         c.set_position(5, 7)
 
@@ -216,6 +225,166 @@ class TestTextEditBuffer(unittest.TestCase):
         self.assertEqual(vr.x, 10)
         self.assertEqual(vr.y, 14)
 
+
+    def test_parented_control_coordinates_negative(self):
+        s = Session()
+        g = GUI(renderer=None, font_descriptor=None, client_session=s)
+
+        c0 = GUIContainer(gui=g)
+        g.content().add_child(c0)
+        c0.set_position(-2, -3)
+
+        c = GUIControl(gui=g)
+        c0.add_child(c)
+        c.set_position(-1, -2)
+
+        r = c.bounding_rect
+        self.assertEqual(r.x, -1)
+        self.assertEqual(r.y, -2)
+
+        wr = c.get_world_rect()
+        self.assertEqual(wr.x, -3)
+        self.assertEqual(wr.y, -5)
+
+        vr = c.get_view_rect()
+        self.assertEqual(vr.x, -3)
+        self.assertEqual(vr.y, -5)
+
+
+    def test_size_to_children_0(self):
+        s = Session()
+        g = GUI(renderer=None, font_descriptor=None, client_session=s)
+
+        cont = GUIContainer(gui=g)
+        g.content().add_child(cont)
+
+        c0 = GUIControl(gui=g)
+        cont.add_child(c0)
+        c0.set_position(5, 7)
+        c0.set_size(10, 20, updateLayout=False)
+
+        r_c0 = c0.bounding_rect
+        self.assertEqual(r_c0.x, 5)
+        self.assertEqual(r_c0.y, 7)
+
+        c1 = GUIControl(gui=g)
+        cont.add_child(c1)
+        c1.set_position(31, 37)
+        c1.set_size(10, 20, updateLayout=False)
+
+        r_c1 = c1.bounding_rect
+        self.assertEqual(r_c1.x, 31)
+        self.assertEqual(r_c1.y, 37)
+
+        self.assertEqual(cont.bounding_rect.x, 0)
+        self.assertEqual(cont.bounding_rect.y, 0)
+
+        # !!!!
+        cont.sizeToChildren(inset_x=0, inset_y=0)
+        r_cont = cont.bounding_rect
+
+        self.assertEqual(r_cont.x, r_c0.x)
+        self.assertEqual(r_cont.y, r_c0.y)
+
+
+    def test_world_to_view_00(self):
+        s = Session()
+        g = GUI(renderer=None, font_descriptor=None, client_session=s)
+
+        wx, wy = 0, 0
+        vx, vy = g.world_to_view(wx, wy)
+        self.assertEqual((vx, vy), (wx, wy))
+
+
+    def test_world_to_view_01(self):
+        s = Session()
+        g = GUI(renderer=None, font_descriptor=None, client_session=s)
+
+        wx, wy = 5, 7
+        vx, vy = g.world_to_view(wx, wy)
+        self.assertEqual((vx, vy), (wx, wy))
+
+
+    def test_world_to_view_02(self):
+        s = Session()
+        g = GUI(renderer=None, font_descriptor=None, client_session=s)
+
+        wx, wy = -5, -7
+        vx, vy = g.world_to_view(wx, wy)
+        self.assertEqual((vx, vy), (wx, wy))
+
+
+    def test_world_to_view_03(self):
+        s = Session()
+        g = GUI(renderer=None, font_descriptor=None, client_session=s)
+
+        g.set_view_pos(5, 7)
+        wx, wy = 0, 0
+        vx, vy = g.world_to_view(wx, wy)
+        self.assertEqual((vx, vy), (-5, -7))
+
+
+    def test_view_to_world_00(self):
+        s = Session()
+        g = GUI(renderer=None, font_descriptor=None, client_session=s)
+
+        vx, vy = 0, 0
+        wx, wy = g.view_to_world(vx, vy)
+        self.assertEqual((wx, wy), (vx, vy))
+
+    
+    def test_view_to_world_01(self):
+        s = Session()
+        g = GUI(renderer=None, font_descriptor=None, client_session=s)
+
+        vx, vy = 5, 7
+        wx, wy = g.view_to_world(vx, vy)
+        self.assertEqual((wx, wy), (vx, vy))
+
+
+    def test_view_to_world_02(self):
+        s = Session()
+        g = GUI(renderer=None, font_descriptor=None, client_session=s)
+
+        vx, vy = -5, -7
+        wx, wy = g.view_to_world(vx, vy)
+        self.assertEqual((wx, wy), (vx, vy))
+
+
+    def test_view_to_world_03(self):
+        s = Session()
+        g = GUI(renderer=None, font_descriptor=None, client_session=s)
+
+        g.set_view_pos(5, 7)
+        wx, wy = g.view_to_world(0, 0)
+        self.assertEqual((wx, wy), (5, 7))
+
+
+    def test_view_to_world_04(self):
+        s = Session()
+        g = GUI(renderer=None, font_descriptor=None, client_session=s)
+
+        g.set_view_pos(-5, -7)
+        wx, wy = g.view_to_world(0, 0)
+        self.assertEqual((wx, wy), (-5, -7))
+
+
+    def test_view_to_world_05(self):
+        s = Session()
+        g = GUI(renderer=None, font_descriptor=None, client_session=s)
+
+        g.set_view_pos(5, 7)
+        wx, wy = g.view_to_world(5, 7)
+        self.assertEqual((wx, wy), (10, 14))
+
+
+    def test_view_to_world_06(self):
+        s = Session()
+        g = GUI(renderer=None, font_descriptor=None, client_session=s)
+
+        g.set_view_pos(-5, -7)
+        wx, wy = g.view_to_world(-5, -7)
+        self.assertEqual((wx, wy), (-10, -14))
 
 
 if __name__ == '__main__':
