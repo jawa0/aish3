3ff6200013d7b428138a3aea5832745ca7ddaef7
Jaba Adams <jaba.adams@gmail.com>
2024-02-17 15:22:52 -0500

    Move agent event creation out of agent.py and into agent_events.py

--- agent.py
+++ agent.py
@@ -1,3 +1,4 @@
+from agent_events import AgentEvents
 import asyncio
 from datetime import datetime
 from event_stream import EventStream
@@ -23,7 +24,7 @@ class Agent:
 
 
     def start(self) -> None:
-        event = self._create_event("SessionStart")
+        event = AgentEvents.create_event("SessionStart")
         self._event_stream.put(event)
 
         loop = asyncio.get_running_loop()
@@ -31,7 +32,7 @@ class Agent:
 
 
     def stop(self) -> None:
-        event = self._create_event("SessionEnd")
+        event = AgentEvents.create_event("SessionEnd")
         self._event_stream.put(event)
 
         if self._task is not None:
@@ -45,7 +46,7 @@ class Agent:
 
     def memorize_text(self, text: str) -> uuid.UUID:
         mem_uid = self.memory.store(memory=Memory(text=text))
-        event = self._create_event("MemorizedText", mem_uid=str(mem_uid), text=text)
+        event = AgentEvents.create_event("MemorizedText", mem_uid=str(mem_uid), text=text)
         self._event_stream.put(event)
         return mem_uid
 
@@ -53,7 +54,7 @@ class Agent:
     def recall_text_by_similarity(self, text: str) -> [Tuple[float, "Memory"]]:
         results = self.memory.retrieve_by_similarity(text=text)
         for s, m in results:
-            event = self._create_event(
+            event = AgentEvents.create_event(
                 "RememberedText",
                 mem_uid=str(m.uid),
                 similarity=s,
@@ -68,32 +69,7 @@ class Agent:
         while True:
             print('Agent._go() ...')
 
-            event = self._create_event("TimeUpdate")
+            event = AgentEvents.create_event("TimeUpdate")
             self._event_stream.put(event)
 
             await asyncio.sleep(TIME_UPDATE_INTERVAL_SECONDS)
-
-
-    def _create_event(self, event_type: str, **kwargs):
-        event = {
-            "version": 0.1,
-            "type": event_type,
-            "user": getpass.getuser(),
-            "client_platform": str(platform.platform()),
-            **self._get_time_metadata(),
-            **kwargs
-        }
-        return event
-
-
-    def _get_time_metadata(self):
-        now_utc = datetime.now(pytz.utc)
-        tz_local = get_localzone()
-        now_local = now_utc.astimezone(tz_local)
-
-        time_details = {
-            "client_timezone": str(tz_local),
-            "client_utc_time": str(now_utc),
-            "client_local_time": str(now_local),
-        }
-        return time_details
